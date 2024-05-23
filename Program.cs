2023b1908475b1a2b11f151e08b0de96f19a02fa
olivleitao <olivleitao@gmail.com>
2024-05-23 11:28:47 +0100

    Middleware Component

--- Program.cs
+++ Program.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Text.Json.Serialization;
+using WebApi.Extensions;
 using WebApi.Helpers;
 using WebApi.Services;
-using WebApi.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -84,6 +85,34 @@ var app = builder.Build();
     app.UseCors("CorsPolicy");
 
     app.UseAuthorization();
+
+    app.Use(async (context, next) => 
+    { 
+        Console.WriteLine($"Logic before executing the next delegate in the Use method"); 
+        await next.Invoke(); Console.WriteLine($"Logic after executing the next delegate in the Use method"); 
+    });
+    app.Map("/usingmapbranch", builder => 
+    { 
+        builder.Use(async (context, next) => { 
+            Console.WriteLine("Map branch logic in the Use method before the next delegate"); 
+            await next.Invoke(); 
+            Console.WriteLine("Map branch logic in the Use method after the next delegate"); }); 
+        builder.Run(async context => { 
+            Console.WriteLine($"Map branch response to the client in the Run method"); 
+            await context.Response.WriteAsync("Hello from the map branch."); 
+        }); 
+    });
+    app.MapWhen(context => context.Request.Query.ContainsKey("testquerystring"), builder => {
+        builder.Run(async context => {
+            await context.Response.WriteAsync("Hello from the MapWhen branch.");
+        }); 
+    });
+    app.Run(async context => 
+    {
+        Console.WriteLine($"Writing the response to the client in the Run method"); 
+        context.Response.StatusCode = 200; 
+        await context.Response.WriteAsync("Hello from the middleware component.");
+    });
 
     app.MapControllers();
 }
